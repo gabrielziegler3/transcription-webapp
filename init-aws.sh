5c585f51d0ec389d85a7f09c36fb5c1a323542da
Gabriel <gabrielziegler3@gmail.com>
2023-09-05 10:37:43 +0300

    Update lambda to process file to bucket

--- init-aws.sh
+++ init-aws.sh
@@ -16,6 +16,12 @@ echo "============ Creating CloudFormation stack... ============"
 # Create the stack using the CloudFormation template
 aws --endpoint-url=http://localhost:4566 --region $AWS_REGION cloudformation create-stack --stack-name my-stack --template-body file:///infra/infrastructure.yaml
 
+echo "============ ARN for upload file SQS ============"
+
+UPLOAD_FILE_SQS_ARN=$(aws --endpoint-url=http://localhost:4566 sqs get-queue-attributes --region "$AWS_REGION" \
+                      --attribute-name QueueArn --queue-url=http://localhost:4566/000000000000/"$UPLOAD_FILE_SQS"\
+                      |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
+
 echo "============ Zipping the Lambda function... ============"
 
 # Create the ZIP file
@@ -24,12 +30,6 @@ zip -r /lambda/lambda.zip /lambda/
 echo "============ Inserting lambda zip into Lambda bucket ============"
 
 aws --endpoint-url=http://localhost:4566 --region $AWS_REGION s3 cp /lambda/lambda.zip s3://$LAMBDA_BUCKET/lambda.zip
-
-echo "============ ARN for upload file SQS ============"
-
-UPLOAD_FILE_SQS_ARN=$(aws --endpoint-url=http://localhost:4566 sqs get-queue-attributes --region "$AWS_REGION" \
-                      --attribute-name QueueArn --queue-url=http://localhost:4566/000000000000/"$UPLOAD_FILE_SQS"\
-                      |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
 
 echo "============ Set S3 Bucket Notification ============"
 
--- lambda/handler.py
+++ lambda/handler.py
@@ -1,12 +1,34 @@
+import boto3
 import json
+import datetime
 
 def lambda_handler(event, context):
+    s3_client = boto3.client('s3', endpoint_url='http://localstack:4566')
+
     for record in event['Records']:
         # Extract the S3 bucket name and object key from the SQS message
         bucket_name = record['s3']['bucket']['name']
         object_key = record['s3']['object']['key']
 
         print(f"File uploaded: {object_key} in bucket {bucket_name}")
+
+        # Prepare the result file content
+        result_content = f"| {object_key} | {datetime.datetime.now().isoformat()} |\n"
+
+        # Define the new bucket name and result file name
+        result_bucket_name = 'processing-results'
+        result_file_name = 'result_file.csv'
+
+        # Check if the result file already exists and append to it
+        try:
+            existing_file = s3_client.get_object(Bucket=result_bucket_name, Key=result_file_name)
+            existing_content = existing_file['Body'].read().decode('utf-8')
+            result_content = existing_content + result_content
+        except:
+            pass
+
+        # Write the result file to the new S3 bucket
+        s3_client.put_object(Bucket=result_bucket_name, Key=result_file_name, Body=result_content)
 
     return {
         'statusCode': 200,
