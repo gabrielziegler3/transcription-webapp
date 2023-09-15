8d46fad00a0353de26196650adf43361af05717b
Gabriel Ziegler <gabrielziegler3@gmail.com>
2023-09-15 15:46:04 +0300

    Update lambda config to debug in-flight messages
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -2,6 +2,11 @@ import boto3
 import json
 import datetime
 import os
+import logging
+
+# Configure logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)  # Set the desired log level
 
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
@@ -9,18 +14,23 @@ def lambda_handler(event, context):
     # Read environment variables for result bucket name
     result_bucket_name = os.environ.get('RESULT_BUCKET_NAME', 'processed-files')
 
+    logger.info(event)
+
     for record in event['Records']:
         # Extract the S3 bucket name and object key from the SQS message
         bucket_name = record['s3']['bucket']['name']
         object_key = record['s3']['object']['key']
 
-        print(f"File uploaded: {object_key} in bucket {bucket_name}")
+        # Log information to CloudWatch Logs
+        logger.info(f"File uploaded: {object_key} in bucket {bucket_name}")
 
         # Prepare the result file content
         result_content = f"| {object_key} | {datetime.datetime.now().isoformat()} |\n"
 
+        now = datetime.datetime.now()
+
         # Define the result file name
-        result_file_name = 'result_file.csv'
+        result_file_name = f'result_file_{now}.csv'
 
         # Check if the result file already exists and append to it
         try:
