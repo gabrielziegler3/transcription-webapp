d1720d0683091d611b3cfd3e0cebbad6759c6b9d
Gabriel Ziegler <gabrielziegler3@gmail.com>
2023-09-18 13:06:33 +0300

    Fix lambda to parse bucket information from SQS message
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -8,42 +8,57 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)  # Set the desired log level
 
+
 def lambda_handler(event, context):
     s3_client = boto3.client('s3')
 
     # Read environment variables for result bucket name
-    result_bucket_name = os.environ.get('RESULT_BUCKET_NAME', 'processed-files')
+    result_bucket_name = os.environ.get(
+        'RESULT_BUCKET_NAME', 'processed-files')
 
     logger.info(event)
 
-    for record in event['Records']:
-        # Extract the S3 bucket name and object key from the SQS message
-        bucket_name = record['s3']['bucket']['name']
-        object_key = record['s3']['object']['key']
+    try:
+        for record in event['Records']:
+            body = json.loads(record['body'])
 
-        # Log information to CloudWatch Logs
-        logger.info(f"File uploaded: {object_key} in bucket {bucket_name}")
+            # Loop through S3 records in the SQS message
+            for s3_record in body['Records']:
+                # Extract the S3 bucket name and object key from the SQS message
+                bucket_name = s3_record["s3"]["bucket"]["name"]
+                object_key = s3_record["s3"]["object"]["key"]
+                # file_size = body["Records"][0]["s3"]["object"]["size"]
 
-        # Prepare the result file content
-        result_content = f"| {object_key} | {datetime.datetime.now().isoformat()} |\n"
+                # Log information to CloudWatch Logs
+                logger.info(
+                    f"File uploaded: {object_key} in bucket {bucket_name}")
 
-        now = datetime.datetime.now()
+                # Prepare the result file content
+                result_content = f"| {object_key} | {datetime.datetime.now().isoformat()} |\n"
 
-        # Define the result file name
-        result_file_name = f'result_file_{now}.csv'
+                now = datetime.datetime.now()
 
-        # Check if the result file already exists and append to it
-        try:
-            existing_file = s3_client.get_object(Bucket=result_bucket_name, Key=result_file_name)
-            existing_content = existing_file['Body'].read().decode('utf-8')
-            result_content = existing_content + result_content
-        except:
-            pass
+                # Define the result file name
+                result_file_name = f'result_file_{now}.csv'
 
-        # Write the result file to the new S3 bucket
-        s3_client.put_object(Bucket=result_bucket_name, Key=result_file_name, Body=result_content)
+                # Check if the result file already exists and append to it
+                try:
+                    existing_file = s3_client.get_object(
+                        Bucket=result_bucket_name, Key=result_file_name)
+                    existing_content = existing_file['Body'].read().decode(
+                        'utf-8')
+                    result_content = existing_content + result_content
+                except:
+                    pass
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Processed message successfully')
-    }
+                # Write the result file to the new S3 bucket
+                s3_client.put_object(
+                    Bucket=result_bucket_name, Key=result_file_name, Body=result_content)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Processed message successfully')
+        }
+    except Exception as e:
+        logger.error(e)
+        raise e
